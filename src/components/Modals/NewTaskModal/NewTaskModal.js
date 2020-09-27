1c2d7bff5a6519fd4b4d7dfd72c8d3da1ecd3a13
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2020-09-27 22:08:12 +0200

    feat: added simple modal state changer

--- src/components/Modals/NewTaskModal/NewTaskModal.js
+++ src/components/Modals/NewTaskModal/NewTaskModal.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+//import Task from "../../../models/taskModel";
 
 const NewTaskModal = ({ visible }) => {
-  return visible ? <div>cześć</div> : null;
+  let [TaskName, newTaskName] = useState("");
+  let [TaskFinalDate, newTaskFinalDate] = useState("");
+
+  const nameHandler = (e) => {
+    newTaskName(e.target.value);
+    console.log(TaskName);
+  };
+
+  const finalDateHandler = (e) => {
+    newTaskFinalDate(e.target.value);
+    console.log(e.target.value);
+    console.log(TaskFinalDate);
+  };
+
+  return visible ? (
+    <form
+      onSubmit={() => {
+        alert(`${TaskFinalDate}, ${TaskName}`);
+      }}
+    >
+      <div>
+        <label htmlFor="taskName">Nazwa zadania</label>
+        <input
+          type="text"
+          id="taskName"
+          //value={newTask.name}
+          onChange={nameHandler}
+        />
+      </div>
+      <div>
+        <label htmlFor="taskDate">Termin na wykonanie zadania</label>
+        <input type="date" id="taskDate" onChange={finalDateHandler} />
+      </div>
+      <input type="submit" value="Dodaj zadanie" />
+    </form>
+  ) : null;
 };
 
 export default NewTaskModal;
