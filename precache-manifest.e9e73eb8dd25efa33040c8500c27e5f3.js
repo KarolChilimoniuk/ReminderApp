079221451b38253903165053f190d67cbcea4d60
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2020-10-27 21:49:03 +0100

    Updates

---
+++ precache-manifest.e9e73eb8dd25efa33040c8500c27e5f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f69ef6cfd24e0342295d1cd7a65d3a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c487e210b097f279d44",
+    "url": "/static/css/main.a172b95c.chunk.css"
+  },
+  {
+    "revision": "f5ef61e9b2e260d543c9",
+    "url": "/static/js/2.01830e90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.01830e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c487e210b097f279d44",
+    "url": "/static/js/main.b2ba06a5.chunk.js"
+  },
+  {
+    "revision": "10545e6b10910a0b9f12",
+    "url": "/static/js/runtime-main.025be44c.js"
+  },
+  {
+    "revision": "49cbfff07c67b122f00bebbd001a3f4d",
+    "url": "/static/media/error.49cbfff0.svg"
+  },
+  {
+    "revision": "3b58b610865fa8b763c8ceca19a115eb",
+    "url": "/static/media/idea.3b58b610.jpg"
+  }
+]);
